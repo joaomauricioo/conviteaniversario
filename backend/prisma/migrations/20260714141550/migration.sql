e3a618fa6eba88c905394665f2868dc901efdce6
João Maurício <joaomauriciopa@gmail.com>
2026-07-14 11:16:25 -0300

    del
    del

---
+++ backend/prisma/migrations/20260714141550/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "administradores" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "convidados" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "presentes" ALTER COLUMN "id" DROP DEFAULT;
